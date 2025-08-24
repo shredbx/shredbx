912ad8b73704e39df9775a85ff85dbb061650855
Andrei Solovev <vault087@gmail.com>
2025-08-24 18:48:39 +0700

    downgrade mcp apadter

--- src/apps/reactbook-web/next.config.ts
+++ src/apps/reactbook-web/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@reactbook/ui-web"],
-  turbopack: {
-    root: "../../..",
-  },
 };
 
 export default nextConfig;
